d5b5310b509f06ab95f980b057f6fc593dd229ee
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-06 16:20:33 -0700

    Add files via upload

--- main.kts
+++ main.kts
@@ -1,6 +1,6 @@
 // Marvish Chandra
 class livingExpenses{
-    fun averageYearly(monthlySalary,basicUtilities = 300,monthlyInternet, monthlyCellular){
+    fun averageYearly(monthlySalary,basicUtilities = 300,monthlyInternet, monthlyCellular)
         rent = 2,796 * 12 
         carLease = (30,000) / 12 * monthlySalary 
         utilities = basicUtilities + monthlyInternet + monthlyCellular
@@ -15,10 +15,10 @@ class livingExpenses{
         ownpet = 100 * 12
         subscriptions = 300
         ccDebt = 5,300 * 12
-        mspouseCost = carLease + groceries + menClothing + menHealth + mCarinsurance + travelingCost + subscriptions + ccDebt 
-        fspouseCost = carLease + groceries + femClothing + femHelth + fCarinsurance + travelingCost + subscriptions + ccDebt
-        married_mSpouse = mspouseCost + first_child_cost + second_child_cost
-        married_fSpouse = fspouseCost + first_child_cost + second_child_cost
+        mCost = carLease + groceries + menClothing + menHealth + mCarinsurance + travelingCost + subscriptions + ccDebt 
+        fCost = carLease + groceries + femClothing + femHelth + fCarinsurance + travelingCost + subscriptions + ccDebt
+        married_mSpouse = mCost + first_child_cost + second_child_cost
+        married_fSpouse = fCost + first_child_cost + second_child_cost
         preschoolCost = 1,135 * 12
         primaryschool_cost = 16,102 * 12
         public_secondary_cost = 30,000 * 12
@@ -27,15 +27,13 @@ class livingExpenses{
         second_child_cost = preschoolCost + primaryschool_cost
         marriedYearly = married_mSpouse + married_fSpouse + rent 
         unmarriedYearly = mspouseCost + fspouseCost + rent
-        mIndividual = mspouseCost + rent + ownpet
-        fIndividual = fspouseCost + rent + ownpet
-    }
-    fun main(){
+        mIndividual = mCost + rent + ownpet
+        fIndividual = fCost + rent + ownpet
+    fun main()
         print(mIndividual)
         print(fIndividual)
         print(mspouseCost)
         print(fspouseCost)
         print(marriedYearly)
         print(unmarriedYearly)
-    }
 }
